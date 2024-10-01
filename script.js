d1dbe3f639ee8fd24533ebcf2003f9d46da9d4ed
anet-eny <anet.eny@gmail.com>
2024-10-01 21:22:46 +0200

    Fetch data from API and create html structure for movie item

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// fetch("http://www.omdbapi.com/?s=spiderman&apikey=d298a8bb")
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data)
+//     })
+
+async function loadMovies(searchTerm) {
+    const url = `http://www.omdbapi.com/?s=${searchTerm}&apikey=d298a8bb`
+    const res = await fetch(`${url}`)
+    const data = await res.json()
+    console.log(data)
+}
+
+loadMovies('star wars')
