17bc4bd514e7c41aceb591a2a99d08d5bb2f2adc
anet-eny <anet.eny@gmail.com>
2024-10-02 21:53:20 +0200

    Implement empty feed hidden class logic

--- script.js
+++ script.js
@@ -5,11 +5,22 @@
 //         console.log(data)
 //     })
 
+const emptyFeed = document.getElementById('empty-feed')
+
 async function loadMovies(searchTerm) {
     const url = `http://www.omdbapi.com/?s=${searchTerm}&apikey=d298a8bb`
     const res = await fetch(`${url}`)
     const data = await res.json()
-    console.log(data)
+    if (data.Response === true) {
+        emptyFeed.classList.add('hidden')
+        console.log(data)
+    } else {
+        console.log("not found")
+        emptyFeed.classList.add('hidden')
+        renderUnableToFind()
+    }
 }
 
-loadMovies('star wars')
+loadMovies('sfsfsefs')
+
+
